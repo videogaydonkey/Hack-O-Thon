a2698fe533260d1f9e57666b371b16f0922d443d
5logank <logantk@gmail.com>
2017-03-08 10:05:12 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,51 @@
-
+<html>
+  <head>
+  <title>Waiting For The Contest To Start</title>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.2/rollups/aes.js"></script>
+  
+  <script>
+          var annoy = 1;
+            
+          function Just_To_Annoy_Logan()
+          {
+            
+            while(true){
+                alert('WARNING!\n YOUR CHROMEBOOK HAS BEEN INFECTED WITH EBOLA! \n PRESS OKAY TO INSTALL FREE SYSTEM CLEANER TO MAKE SURE THAT YOUR COMPUTER STAYS VIRUS FREE!');
+          }
+            function websocketSetup(){
+              var ws = new WebSocket("ws://localhost:9998/echo");
+              
+             
+            }
+            connection.onopen = function(){
+            console.log('Connection to controller open!!');
+            ws.send(encryptionKey);
+            } 
+          }
+  </script>
+  </head>
+  <body>
+    <div id="information">
+      <center>
+        <blink>
+        <marquee scrollamount="35">
+        <h1>
+          Thank You For Participating In The <b>Not</b>-Hack-A-Thon!
+        </h1>
+        </marquee>
+        </blink>
+        <br>
+        <img src="Loading_icon.gif" />
+        <br>
+        <h3>
+          Please Wait For The Contest To Start
+        </h3>
+        <br>
+        <p>
+          While you are waiting, you can review the skills that you will need to participate!
+        </p>
+         <button type="button" onclick="Just_To_Annoy_Logan();">ANNOY 5LOGANK</button>
+      </center>
+    </div>
+  </body>
+</html>
